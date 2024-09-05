5aacea7e81baf61740200d9ac332b7bbb5ab29a3
Liamlunkamba <liamlunkamba12@gmail.com>
2024-09-05 09:32:25 -0400

    I liike

--- src/main/java/com/example/demo/TutorialRepository/TutorialRepository.java
+++ src/main/java/com/example/demo/TutorialRepository/TutorialRepository.java
@@ -1,16 +1,17 @@
 package com.example.demo.TutorialRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.demo.model.Tutorial;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource(path = "learningtutorials")
 
 @Repository
 public interface TutorialRepository extends JpaRepository<Tutorial, Long>
      // Tutorial → Entity type and Long --> primary key
 {
+
      //   findByPublished is a custom method()
     List<Tutorial> findByPublished(boolean published);
 }
